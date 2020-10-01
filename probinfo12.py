1db276d3d3e1fd06bc142c32d292df8f9d7aa98b
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-10-01 14:36:43 +0300

    Add files via upload

---
+++ probinfo12.py
@@ -0,0 +1,5 @@
+print("problema12")
+a=int(input("Introduceti nr. de oua pe care le face prima gaina timp de o zi:"))
+b=int(input("Introduceti nr. de oua pe care le face a doua gaina timp de o zi:"))
+c=int(input("Introduceti nr. de oua pe care le face a treia gaina timp de o zi:"))
+print("Intr-o saptamina se obtin :", 7*(a+b+c), "oua")
